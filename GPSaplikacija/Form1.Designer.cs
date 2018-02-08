87da1dc3a3ad7487c1f1116727b86726eab6dd13
ivceh <ivan.ceh1234@gmail.com>
2018-02-08 09:47:41 +0100

    Početak klase čvor

---
+++ GPSaplikacija/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace GPSaplikacija
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1017, 501);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "GPS aplikacija";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ GPSaplikacija/Čvor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPSaplikacija
+{
+    class Čvor
+    {
+        double x;
+        double y;
+        //SortedSet<Brid> skupSusjednihBridova;
+
+        public Čvor(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
